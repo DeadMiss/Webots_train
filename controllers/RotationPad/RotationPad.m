1d3a5f4d190724c5ab15bcc080f633f84406feb5
228688 <74045337+228688@users.noreply.github.com>
2020-12-18 17:44:47 +0100

    Objektyy

--- controllers/RotationPad/RotationPad.m
+++ controllers/RotationPad/RotationPad.m
@@ -18,19 +18,24 @@ TIME_STEP = 64;
 
 horny_motor = wb_robot_get_device('horny_motor');
 wb_motor_set_position(dolny_motor, inf);
-wb_motor_set_velocity(dolny_motor, 0);
+wb_motor_set_velocity(dolny_motor, 1);
 
 dolny_motor = wb_robot_get_device('dolny_motor');
+<<<<<<< Updated upstream
 wb_motor_set_position(dolny_motor, inf);
 wb_motor_set_velocity(dolny_motor, 0);
+=======
+wb_motor_set_position(spodny_motor, inf);
+wb_motor_set_velocity(spodny_motor, 1);
+>>>>>>> Stashed changes
 
 lavy_motor = wb_robot_get_device('lavy_motor');
 wb_motor_set_position(lavy_motor, inf);
-wb_motor_set_velocity(lavy_motor, 0);
+wb_motor_set_velocity(lavy_motor, 1);
 
 pravy_motor = wb_robot_get_device('pravy_motor');
 wb_motor_set_position(pravy_motor, inf);
-wb_motor_set_velocity(pravy_motor, 0);
+wb_motor_set_velocity(pravy_motor, 1);
 
 
 Sensor = wb_robot_get_device('LightSensor');
